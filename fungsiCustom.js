c5169f66c4e378dd2398ea26769b4f4de1165c96
FebryES <febry.ermansyah86@gmail.com>
2022-06-12 18:56:37 +0700

    tugas6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs')
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,55 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let resultArray = []
+  
+  fs.readFile(
+    file1,
+    {
+      encoding : "utf8"
+    },
+    (err, data)=> {
+      if (err) {
+        return fnCallBack(err, null);
+      }
 
+      const dataResult = JSON.parse(data)
+      resultArray.push(dataResult.message.split(" ")[1])
+    }
+  )
+
+  fs.readFile(
+    file2,
+    {
+      encoding : "utf8"
+    },
+    (err, data)=>{
+      if (err) {
+        return fnCallBack(err, null);
+      }
+
+      const dataResult = JSON.parse(data)
+      resultArray.push(dataResult[0].message.split(" ")[1])
+    }
+  )
+
+  fs.readFile(
+    file3,
+    {
+      encoding : "utf8"
+    },
+    (err, data) => {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+
+      const dataResult = JSON.parse(data)
+      resultArray.push(dataResult[0].data.message.split(" ")[1])
+      fnCallBack(null, resultArray)
+    }
+  )
+}
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
